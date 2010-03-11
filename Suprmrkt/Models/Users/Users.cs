8180b6e3a44f57484304715096b5f52fabb4a948
unknown <Xiy@.(none)>
2010-03-11 19:11:15 +0000

    Database connection updates..

--- Suprmrkt/Models/Users/Users.cs
+++ Suprmrkt/Models/Users/Users.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Suprmrkt.Controllers;
+using System.Data.SQLite;
+using System.Data;
 
 namespace Suprmrkt.Models.Users
 {
 	class Users
 	{
+		private SQLiteController sqlController;
+
+		public Users(SQLiteController sql)
+		{
+			this.sqlController = sql;
+		}
+
 		public void GetPasswordForUser(UserType user)
 		{
+			SQLiteCommand sqlCmd = new SQLiteCommand();
+			sqlCmd.Connection = sqlController.SQLConnection;
 			switch (user)
 			{
 				case UserType.Advanced:
-					
+					sqlCmd.CommandText = "SELECT password FROM users WHERE (username == 'advanced')";
+					SQLiteDataReader sqlReader = sqlCmd.ExecuteReader();
+					sqlReader.GetString(0);
 					break;
 				case UserType.Standard:
 					break;
