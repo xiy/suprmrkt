f371225d7eed13b2abfb2aa4fda7bffcb1a996a7
xiy <xiy3x0@gmail.com>
2010-03-12 07:53:15 +0000

    Added SimulationController and Simulation tests.

---
+++ Suprmrkt/Controllers/SimulationController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Suprmrkt.Interfaces;
+
+namespace Suprmrkt.Controllers
+{
+	class SimulationController : ControllerBase, IController
+	{
+		#region Singleton
+		// Lazy loading implementation of the singleton pattern
+		SimulationController() { }
+		static readonly SimulationController _instance = new SimulationController();
+
+		public static SimulationController Instance
+		{
+			get
+			{
+				return NestedSingleton._instance;
+			}
+		}
+
+		class NestedSingleton
+		{
+			static NestedSingleton() { }
+			internal static SimulationController _instance = new SimulationController();
+		} 
+		#endregion
+
+		#region IController Members
+
+
+		public IView View
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public IModel Model
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		#endregion
+	}
+}
